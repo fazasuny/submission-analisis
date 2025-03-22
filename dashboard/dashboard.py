18a21f39f5e9ff0171f26044842c4b275c3d08c9
fazasuny <sunyfaza@gmail.com>
2025-03-22 13:52:11 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy
+import os
 
 # Load dataset
 pd.read_csv(os.path.join(os.path.dirname(__file__), "main-data.csv"))
